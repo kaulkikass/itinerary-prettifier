430799d326407159eb32fd82e0cdfe452f9abd25
kaulkikas <kaulkikass@gmail.com>
2025-09-15 22:00:25 +0300

    Add input.txt reading

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
-public class Main {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+
+        //Reading input to list, converting to string
+        try {
+        List<String> lines = Files.readAllLines(Path.of("input.txt"));
+        String input = String.join("\n", lines);
+        System.out.println(input);
+        } catch (IOException e) {
+            System.out.println("Input not found");
+        }
+
+        
+
+
+    }
 }
